d2816ee424c27caaf36a090b3ca2c2be0365c02e
Sean Cowans <zedesia@yahoo.com>
2023-01-04 15:09:49 -0700

    Call methos

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,13 @@
 group_7 = ["Sean" , "Grace" , "Angel" , "Margaret"]
 ages = [21 , 22, 23, 24]
-heights = [23.4 , 13.3 , 78.2 , 9.1]
-is cool - [true , true , true , true]
+heights = [23.4, 13.3, 78.2, 9.1]
+is_cool = [true, true, true, true]
+ group_7.pop
+#will remove last name
+ ages.push(12)
+#will add 12 to the end of ages 
+ heights.shift(2)
+#will remove the first two heights
+is_cool.unshift(false)
+#will add false to the front of the array
+
